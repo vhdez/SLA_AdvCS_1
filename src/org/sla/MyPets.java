78eedd64f705cf0fed7b9529685c52d6c22fede3
vhernandez <vhernandez@scienceleadership.org>
2018-09-13 00:26:37 -0400

    Added petType.

--- src/org/sla/MyPets.java
+++ src/org/sla/MyPets.java
@@ -6,11 +6,11 @@ class MyPets {
         // I have 3 pets.
 
         // The 1st pet is a dog named Rufus.
-        Dog rufus = new Dog();
-        rufus.age = 3;
-        rufus.name = "Rufus";
-        rufus.petType = "dog";
-        rufus.eatsDryFood = false;
+        Dog bowser = new Dog();
+        bowser.age = 3;
+        bowser.name = "Bowser";
+        bowser.petType = "dog";
+        bowser.eatsDryFood = false;
 
         // The 2nd pet is an older dog named Tully.
         Dog tully = new Dog();
@@ -28,14 +28,14 @@ class MyPets {
 
         // Tell the pets to say hi.
         System.out.println("SAY HI PETS!");
-        rufus.sayHi();
+        bowser.sayHi();
         tully.sayHi();
         ralph.sayHi();
         System.out.println();
 
         // Tell the pets to describe themselves.
         System.out.println("NOW DESCRIBE YOURSELVES:");
-        rufus.describe();
+        bowser.describe();
         System.out.println();
         tully.describe();
         System.out.println();
@@ -43,7 +43,7 @@ class MyPets {
         System.out.println();
 
         // Report cumulative data about pets.
-        int totalPetsAge = rufus.getAge() + tully.getAge() + ralph.getAge();
+        int totalPetsAge = bowser.getAge() + tully.getAge() + ralph.getAge();
         System.out.println("My pets have lived a total of " + totalPetsAge + " human years.");
     }
 }
