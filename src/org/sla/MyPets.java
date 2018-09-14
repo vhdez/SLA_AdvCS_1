19fbfa77d66101a54b8fe764b3a7f0b885b9d3b1
andrewrodebaugh <aquahydration@gmail.com>
2018-09-14 13:30:09 -0400

    Iggy and George

--- src/org/sla/MyPets.java
+++ src/org/sla/MyPets.java
@@ -26,11 +26,29 @@ class MyPets {
         ralph.petType = "cat";
         ralph.eatsDryFood = false;
 
+        // The other dog is a dog named George.
+
+        Dog George = new Dog();
+        George.age = 1;
+        George.name = "George";
+        George.petType = "dog";
+        George.eatsDryFood = false;
+
+        // An Iguana named Iggy
+
+        Iguana Iggy = new Iguana();
+        Iggy.age = 5;
+        Iggy.name = "Iggy";
+        Iggy.petType = "Iguana";
+        Iggy.eatsDryFood = false;
+
         // Tell the pets to say hi.
         System.out.println("SAY HI PETS!");
         bowser.sayHi();
         tully.sayHi();
         ralph.sayHi();
+        George.sayHi();
+        Iggy.sayHi();
         System.out.println();
 
         // Tell the pets to describe themselves.
@@ -41,9 +59,12 @@ class MyPets {
         System.out.println();
         ralph.describe();
         System.out.println();
+        George.describe();
+        System.out.println();
+        Iggy.describe();
 
         // Report cumulative data about pets.
-        int totalPetsAge = bowser.getAge() + tully.getAge() + ralph.getAge();
+        int totalPetsAge = bowser.getAge() + tully.getAge() + ralph.getAge() + George.getAge() + Iggy.getAge();
         System.out.println("My pets have lived a total of " + totalPetsAge + " human years.");
     }
 }
